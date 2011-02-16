9bf78977cc0591a866d5bff263491be49a94b51a
Franck D'agostini <franck.dagostini@gmail.com>
2011-02-16 21:06:38 +0100

    correcting rake tasks loading in Rails app

---
+++ lib/sitemap_builder/railtie.rb
@@ -0,0 +1,10 @@
+module SitemapBuilder
+  class Railtie < Rails::Railtie
+    railtie_name :sitemap_builder
+    
+    rake_tasks do
+      load "tasks/sitemap_builder_tasks.rake"
+    end
+
+  end
+end
